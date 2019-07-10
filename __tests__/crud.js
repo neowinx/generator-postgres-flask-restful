d6aa60a97d744b4c7036a7fa352fab6d8d799ca2
Pedro Flores <pdavidflores@gmail.com>
2019-07-10 00:06:18 -0400

    crud tests finally working somehow

--- __tests__/crud.js
+++ __tests__/crud.js
@@ -1,15 +1,15 @@
 'use strict';
+const { spawn } = require('child_process');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
 const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
 const dockerCLI = require('docker-cli-js');
 const Docker = dockerCLI.Docker;
 
 const docker = new Docker();
 
-jest.setTimeout(30000);
+jest.setTimeout(60000);
 
 function sleep(ms) {
   return new Promise(resolve => {
@@ -19,11 +19,12 @@ function sleep(ms) {
 
 const testPath = fs.mkdtempSync(path.join(os.tmpdir(), 'foo-'));
 
+console.log('testPath: ' + testPath);
+
 // WORK IN PROGRESS
-xdescribe('generator-flask-restful:crud', () => {
+describe('generator-flask-restful:crud', () => {
   beforeAll(async done => {
     let schemaPath = path.join(__dirname, 'schema.sql');
-    console.log(testPath);
     console.log('stopping ALL postgres running containers...');
     let psdata = await docker.command('ps');
     await psdata.containerList.forEach(async function(container) {
@@ -43,19 +44,58 @@ xdescribe('generator-flask-restful:crud', () => {
       `exec -t ${nucontainer.containerId} bash -c 'psql -U postgres -f /tmp/schema.sql'`
     );
     console.log('database schema restored');
-    return helpers
-      .run(path.join(__dirname, '../generators/crud'))
-      .inDir(testPath)
-      .withPrompts({
-        host: 'localhost',
-        port: 15432,
-        database: 'db',
-        user: 'postgres',
-        password: 'postgres',
-        schema: 'public',
-        tables: ['public.user']
-      })
-      .on('end', done);
+
+    console.log('spawning yo command...');
+    let ls = await spawn('yo', ['postgres-flask-restful:crud'], {
+      cwd: testPath
+    });
+
+    var expectedAnswers = 0;
+
+    await new Promise(resolve => {
+      ls.stdout.on('data', data => {
+        if (data.indexOf('Ingrese el host (localhost)') > 0) {
+          ls.stdin.write('\n');
+          expectedAnswers++;
+        }
+        if (data.indexOf('Ingrese el puerto (5432)') > 0) {
+          ls.stdin.write('15432\n');
+          expectedAnswers++;
+        }
+        if (data.indexOf('Ingrese el nombre de la base de datos (postgres)') > 0) {
+          ls.stdin.write('db\n');
+          expectedAnswers++;
+        }
+        if (data.indexOf('Ingrese el usuario (postgres)') > 0) {
+          ls.stdin.write('\n');
+          expectedAnswers++;
+        }
+        if (data.indexOf('Ingrese el password (postgres)') > 0) {
+          ls.stdin.write('\n');
+          expectedAnswers++;
+        }
+        if (data.indexOf('Ingrese el esquema al que quiere conectar (public)') > 0) {
+          ls.stdin.write('\n');
+          expectedAnswers++;
+        }
+        if (data.indexOf('Seleccione las tablas a generar artefactos') > 0) {
+          ls.stdin.write('a\n');
+          expectedAnswers++;
+        }
+        if (data.indexOf('a) overwrite this and all others') > 0) {
+          ls.stdin.write('a\n');
+          expectedAnswers++;
+        }
+        if (expectedAnswers >= 9) {
+          console.log('ending prompt input');
+          ls.stdin.end();
+        }
+      });
+      ls.stdout.on('end', () => {
+        resolve();
+      });
+    });
+    done();
   });
 
   it('creates files', () => {
