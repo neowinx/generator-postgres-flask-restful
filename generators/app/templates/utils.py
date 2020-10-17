ffde2721cf10b222a428525f8df3bee1d87dec25
Pedro Flores <pdavidflores@gmail.com>
2020-10-16 22:53:42 -0300

    added better pagination options

--- generators/app/templates/utils.py
+++ generators/app/templates/utils.py
@@ -108,9 +108,16 @@ def check(permision):
 
 
 def paginated_results(query):
-    paginated = query.paginate(page=request.args.get('page', 1, int))
-    return {
-        'page': paginated.page,
-        'pages': paginated.pages,
-        'items': [x.json() for x in paginated.items]
-    }
+    pagination = request.args.get('pagination', 'true', str)
+    jsondepth = request.args.get('jsondepth', 1, int)
+    if pagination == 'true':
+        paginated = query.paginate(page=request.args.get('page', 1, int))
+        return {
+            'page': paginated.page,
+            'pages': paginated.pages,
+            'items': [x.json(jsondepth) if jsondepth else x.json() for x in paginated.items]
+        }
+    else:
+        return [x.json(jsondepth) if jsondepth else x.json() for x in query.all()]
+
+
